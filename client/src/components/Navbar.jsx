51a71ac37eea395b7fb797df87e9d4fefcb80739
Kamran Mohammadi <84287882+kamran1807@users.noreply.github.com>
2024-08-20 18:50:34 +0330

    complete profile page UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
           <Link to='/'><li>Home</li></Link>
           <Link to='/about'><li>About</li></Link>
 
-          <Link to='/sign-in'>
+          <Link to='/profile'>
             {currentUser ? (
               <img src={currentUser.profilePicture} alt='profile image' className='h-7 w-7 rounded-full object-cover' />
             ) : (
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -2,9 +2,7 @@ import { useSelector } from 'react-redux'
 import { Outlet, Navigate } from 'react-router-dom'
 
 export default function PrivateRoute() {
-  const { currentUser } = useSelector((state) => state.user)
-  return (
-    // render children or sign in page
-    currentUser ? <Outlet /> : <Navigate to='/sign-in' />
-  )
+  const { currentUser } = useSelector(state => state.user)
+  // render children or sign in page
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' /> 
 }
